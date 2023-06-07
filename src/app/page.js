3e5f798f39cdb39ad7d24cb406222aa14fcdfbaa
Rubismar Carlos <rubens_fake_05@hotmail.com>
2023-06-07 16:33:42 -0300

    Adicionando banner e css da página principal e mudanças nos card dos pokémons

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 'use client'
 import styles from './page.module.css'
 
-import { useState, useEffect } from 'react'
+import Link from 'next/link'
+
+import { useState } from 'react'
 
 import axios from 'axios'
+
+import { MdOutlineVisibility, MdOutlineVisibilityOff } from 'react-icons/md'
 
 
 export default function Home() {
@@ -13,11 +17,21 @@ export default function Home() {
   const [search, setSearch] = useState(false)
   const [btnOpenHabilidades, setBtnOpenHabilidades] = useState(false)
   const [btnOpenPoderes, setBtnOpenPoderes] = useState(false)
+  const [btnHabilidadeVisible, setBtnHabilidadeVisible] = useState(false)
+  const [btnPoderesVisible, setBtnPoderesVisible] = useState(false)
 
+  // url imagem do banner
+  const urlBanner = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png"
 
-  const handleOpenHabilidades = () => setBtnOpenHabilidades(!btnOpenHabilidades)
+  const handleOpenHabilidades = () => {
+    setBtnOpenHabilidades(!btnOpenHabilidades)
+    setBtnHabilidadeVisible(!btnHabilidadeVisible)
+  }
 
-  const handleOpenPoderes = () => setBtnOpenPoderes(!btnOpenPoderes)
+  const handleOpenPoderes = () => {
+    setBtnOpenPoderes(!btnOpenPoderes)
+    setBtnPoderesVisible(!btnPoderesVisible)
+  }
 
   const handleSearch = async (e) => {
     const term = e.target.value;
@@ -63,50 +77,62 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.nav}>
         <input type="text" value={searchTerm} onChange={handleSearch} placeholder="Digite o nome do Pokémon" />
-        <button onClick={handleOpenHabilidades}>Mostrar Habilidades</button>
-        <button onClick={handleOpenPoderes}>Mostrar Poderes</button>
+      </div>
+      <div>
+        {pokemonList.length > 0 && (
+          <div className={styles.nav_btn}>
+          <button onClick={handleOpenHabilidades} className={styles.btns}>
+            {!btnHabilidadeVisible && <div className={styles.box_svg}>< MdOutlineVisibility /></div>}
+            {btnHabilidadeVisible && <div className={styles.box_svg}>< MdOutlineVisibilityOff /></div>}
+            HABILIDADES
+          </button>
+          <button onClick={handleOpenPoderes} className={styles.btns}>
+            {!btnPoderesVisible && <div className={styles.box_svg}>< MdOutlineVisibility /></div>}
+            {btnPoderesVisible && <div className={styles.box_svg}>< MdOutlineVisibilityOff /></div>}
+            PODERES
+          </button>
+        </div>
+        )}
       </div>
 
       <div className={styles.container_card}>
         <div className={styles.container_card}>
-          {pokemonList.length > 0 ? (
+          {pokemonList.length > 0 && (
             pokemonList.map((pokemon, index) => (
               <div key={index} className={styles.pokemon}>
-              <div className={styles.box_card}>
-                <div className={styles.box_background}>
-                <div className={styles.box_name_img}>
-                  <h2>{pokemon.name}</h2>
-                  <div className={styles.box_card_img}>
-                    <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+                <div className={styles.box_card}>
+                  <div className={styles.box_background}>
+                    <div className={styles.box_name_img}>
+                      <h2>{pokemon.name}</h2>
+                      <div className={styles.box_card_img}>
+                        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+                      </div>
+                    </div>
                   </div>
-                </div>
-                </div>
                 <div>
                   <div className={styles.box_habilidades}>
                     <ul>
-                      <div className={styles.nome_card}>
-                        <p>Habilidades</p>
-                      </div>
                       <div className={`${btnOpenHabilidades ? `${styles.btn_habilidades_ativado}` : `${styles.btn_habilidades_inativo}`}`}>
+                      <div>
                         {pokemon.abilities.map((ability, index) => (
                           <div key={index}>
                               <p>{ability.ability.name}</p>
                           </div>
                         ))}
                       </div>
+                      </div>
                     </ul>
                   </div>
                   <div className={styles.box_poderes}>
                     <ul>
-                      <div className={styles.nome_card}>
-                        <p>Poderes</p>
-                      </div>
                       <div className={`${btnOpenPoderes ? `${styles.btn_poderes_ativado}` : `${styles.btn_poderes_inativo}`}`}>
+                      <div>
                         {pokemon.stats.map((stat, index) => (
                           <div key={index}>
                             <p>{stat.stat.name}: {stat.base_stat}</p>
                           </div>
                         ))}
+                      </div>
                       </div>
                     </ul>
                   </div>
@@ -114,11 +140,18 @@ export default function Home() {
               </div>     
               </div> 
             ))
-          ) : (
-            <p>Digite o nome de um pokemon</p>
           )}
         </div>
       </div>
+      {pokemonList.length === 0 && (
+        <div className={styles.container_banner}>
+            <div className={styles.img_banner}>
+                <img src={urlBanner} alt="Imagem de banner" />
+            </div>
+            <h1>Pesquise seu Pokémon favorito</h1>
+            <p>Veja as habilidades e poderes do seu pokémon</p>
+        </div>
+      )}
     </main>
   )
 }
